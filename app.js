951f6a7bfb5d6b941b5f39778151771f614319a4
Shannon Johnson <shannonj498@gmail.com>
2017-11-20 14:37:55 -0700

    Show navbar on scroll

---
+++ app.js
@@ -0,0 +1,14 @@
+$("#navbar").hide();
+
+  // fade in .navbar
+  $(function () {
+      $(window).scroll(function () {
+
+               // set distance user needs to scroll before we start fadeIn
+          if ($(this).scrollTop() > 600) {
+              $('#navbar').fadeIn(1000);
+          } else {
+              $('#navbar').fadeOut(1000);
+          }
+      });
+  });
